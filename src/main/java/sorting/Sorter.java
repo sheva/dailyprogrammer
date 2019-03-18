5d8ac9265ae498706a4ce0aeef0b28e010aad919
havryliuk <oleksander.havryliuk@gmail.com>
2019-03-18 11:18:23 +0200

    interim changes, committing not to lose

--- src/main/java/sorting/Sorter.java
+++ src/main/java/sorting/Sorter.java
@@ -1,56 +1,52 @@
 package sorting;
 
-import javafx.util.Pair;
-
 import java.util.ArrayList;
 import java.util.List;
 
 class Sorter {
-    List<Integer> mergeSort(List<Integer> input) {
-        List<Integer> result = new ArrayList<>();
+    private List<Integer> numbers;
+    private List<Integer> helper;
 
-
-
-        return result;
+    void sort(List<Integer> values) {
+        this.numbers = values;
+        int size = values.size();
+        this.helper = new ArrayList<>(size);
+        mergesort(0, size - 1);
     }
 
-    Pair<List<Integer>, List<Integer>> split(List<Integer> list) {
-        List<Integer> list1;
-        List<Integer> list2;
-
-        int size = list.size();
-        list1 = list.subList(0, size / 2);
-        list2 = list.subList(size / 2, size);
-
-        return new Pair<>(list1, list2);
+    private void mergesort(int low, int high) {
+        if (low < high) {
+            int middle = low + (high - low) / 2;
+            mergesort(low, middle);
+            mergesort(middle + 1, high);
+            merge(low, middle, high);
+        }
     }
 
-    List<Integer> merge(List<Integer> list1, List<Integer> list2) {
-        int size = list1.size() + list2.size();
-        List<Integer> result = new ArrayList<>(size);
-
-        int list1Pointer = 0;
-        int list2Pointer = 0;
-        for (int i = 0; i < size; i++) {
-            try {
-                if (list1.get(list1Pointer) < list2.get(list2Pointer)) {
-                    result.add(i, list1.get(list1Pointer));
-                    list1Pointer++;
-                } else {
-                    result.add(i, list2.get(list2Pointer));
-                    list2Pointer++;
-                }
-            } catch (ArrayIndexOutOfBoundsException e) {
-                Integer last;
-                if (list1.size() > list2.size()) {
-                    last = list1.get(list1.size() - 1);
-                } else {
-                    last = list2.get(list2.size() - 1);
-                }
-                result.add(size - 1, last);
-            }
+    private void merge(int low, int middle, int high) {
+        for (int i = low; i <= high; i++) {
+            helper.add(i , numbers.get(i));
         }
 
-        return result;
+        int i = low;
+        int j = middle + 1;
+        int k = low;
+
+        while (i <= middle && j <= high) {
+            if (helper.get(i) <= helper.get(j)) {
+                numbers.add(k, helper.get(i));
+                i++;
+            } else {
+                numbers.add(k, helper.get(j));
+                j++;
+            }
+            k++;
+        }
+
+        while (i <= middle) {
+            numbers.add(k, helper.get(i));
+            k++;
+            i++;
+        }
     }
 }
--- src/test/java/sorting/SorterTest.java
+++ src/test/java/sorting/SorterTest.java
@@ -1,10 +1,11 @@
 package sorting;
 
 import com.google.common.collect.Ordering;
-import javafx.util.Pair;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -16,8 +17,10 @@ public class SorterTest {
         List<Integer> input = RandomIntListGenerator.getListOfRandomInts(100000000);
 
         Sorter sorter = new Sorter();
-        List<Integer> result = sorter.mergeSort(input);
-        assertTrue(Ordering.natural().isOrdered(result));
+        System.out.println(LocalDateTime.now() + " Sorting 100 million integers.");
+        sorter.sort(input);
+        System.out.println(LocalDateTime.now() + " 100 million integers sorted.");
+        assertTrue(Ordering.natural().isOrdered(input));
     }
 
     @Test
@@ -25,69 +28,20 @@ public class SorterTest {
         List<Integer> input = RandomIntListGenerator.getListOfRandomInts(1000);
 
         Sorter sorter = new Sorter();
-        List<Integer> result = sorter.mergeSort(input);
-        assertTrue(Ordering.natural().isOrdered(result));
+        System.out.println(LocalDateTime.now() + " Sorting 1000 integers.");
+        sorter.sort(input);
+        System.out.println(LocalDateTime.now() + " 1000 integers sorted.");
+        assertTrue(Ordering.natural().isOrdered(input));
     }
 
     @Test
-    public void mergeTest() {
-        Integer[] array1 = {1, 4, 5, 99, 234, 235, 888};
-        List<Integer> input1 = Arrays.asList(array1);
-        Integer[] array2 = {2, 54, 58, 99, 236, 278, 878, 384957};
-        List<Integer> input2 = Arrays.asList(array2);
-
+    public void smallMergeSort() {
         Integer[] expectedArray = {1, 2, 4, 5, 54, 58, 99, 99, 234, 235, 236, 278, 878, 888, 384957};
-        List<Integer> expected = Arrays.asList(expectedArray);
+        List<Integer> input = Arrays.asList(expectedArray);
+        Collections.shuffle(input);
 
         Sorter sorter = new Sorter();
-        List<Integer> actual = sorter.merge(input1, input2);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void mergeTestOne() {
-        Integer[] array1 = {1};
-        List<Integer> input1 = Arrays.asList(array1);
-        List<Integer> input2 = Arrays.asList(new Integer[] {});
-
-        Integer[] expectedArray = {1};
-        List<Integer> expected = Arrays.asList(expectedArray);
-
-        Sorter sorter = new Sorter();
-        List<Integer> actual = sorter.merge(input1, input2);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void splitTest() {
-        Integer[] inputArray = {2, 54, 58, 99, 150, 236, 278, 878, 384957};
-        List<Integer> input = Arrays.asList(inputArray);
-
-        Sorter sorter = new Sorter();
-        Pair<List<Integer>, List<Integer>> actual = sorter.split(input);
-
-        Integer[] expectedArray1 = {2, 54, 58, 99};
-        List<Integer> expectedList1 = Arrays.asList(expectedArray1);
-        Integer[] expectedArray2 = {150, 236, 278, 878, 384957};
-        List<Integer> expectedList2 = Arrays.asList(expectedArray2);
-        Pair<List<Integer>, List<Integer>> expected = new Pair<>(expectedList1, expectedList2);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void splitTestOne() {
-        Integer[] inputArray = {2};
-        List<Integer> input = Arrays.asList(inputArray);
-
-        Sorter sorter = new Sorter();
-        Pair<List<Integer>, List<Integer>> actual = sorter.split(input);
-
-        List<Integer> expectedList1 = Arrays.asList(new Integer[] {});
-        Integer[] expectedArray2 = {2};
-        List<Integer> expectedList2 = Arrays.asList(expectedArray2);
-        Pair<List<Integer>, List<Integer>> expected = new Pair<>(expectedList1, expectedList2);
-
-        assertEquals(expected, actual);
+        sorter.sort(input);
+        assertTrue(Ordering.natural().isOrdered(input));
     }
 }
