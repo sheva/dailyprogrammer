f090ea4247aea8776faf6cf9446ec781039ae3af
havryliuk <oleksander.havryliuk@gmail.com>
2019-03-22 17:21:04 +0200

    test java 11's String.lines()

--- src/test/java/java11/StringTest.java
+++ src/test/java/java11/StringTest.java
@@ -2,6 +2,9 @@ package java11;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static junit.framework.Assert.assertEquals;
 
 public class StringTest {
@@ -11,5 +14,12 @@ public class StringTest {
         String test = "havryliuk";
         String repeated = test.repeat(5);
         assertEquals("havryliukhavryliukhavryliukhavryliukhavryliuk", repeated);
+    }
+
+    @Test
+    public void testLines() {
+        String multipleLinesString = "First line.\nSecond line.\nThird line.\n";
+        List<String> lines = multipleLinesString.lines().collect(Collectors.toList());
+        assertEquals(3, lines.size());
     }
 }
