8d19ba8f545c1b7d2fe39e86624e069f704e1fd1
havryliuk <oleksander.havryliuk@gmail.com>
2019-03-25 08:55:00 +0200

    more java 11 String new methods

--- src/test/java/java11/StringTest.java
+++ src/test/java/java11/StringTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StringTest {
 
@@ -21,5 +22,42 @@ public class StringTest {
         String multipleLinesString = "First line.\nSecond line.\nThird line.\n";
         List<String> lines = multipleLinesString.lines().collect(Collectors.toList());
         assertEquals(3, lines.size());
+    }
+
+    @Test
+    public void testStripLeading() {
+        String hasLeadingWhiteSpace = " Test  ";
+        String strippedTest = hasLeadingWhiteSpace.stripLeading();
+        assertEquals("Test  ", strippedTest);
+    }
+
+    @Test
+    public void testStripTrailing() {
+        String hasTrailingWhiteSpace = " Test  ";
+        String strippedTest = hasTrailingWhiteSpace.stripTrailing();
+        assertEquals(" Test", strippedTest);
+    }
+
+    @Test
+    public void testStrip() {
+        String hasLeadingAndTrailingWhiteSpace = " Test\t\n  ";
+        String stripped = hasLeadingAndTrailingWhiteSpace.strip();
+        assertEquals("Test", stripped);
+    }
+
+    @Test
+    public void testStrip2() {
+        String test = " te st  ";
+        String actual = test.strip();
+        assertEquals("te st", actual);
+    }
+
+    @Test
+    public void test_isBlank() {
+        assertTrue("".isBlank());
+        assertTrue(System.getProperty("line.separator").isBlank());
+        assertTrue("\t".isBlank());
+        assertTrue("    ".isBlank());
+        assertTrue("\t\n    ".isBlank());
     }
 }
